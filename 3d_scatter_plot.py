972a34373c0af0d651ccab4acf3d1af3cba53ada
hao <hao.coding@dorabot.com>
2018-04-26 11:50:21 +0800

    use numpy for dome and lightvec

--- 3d_scatter_plot.py
+++ 3d_scatter_plot.py
@@ -19,27 +19,46 @@ class LightVector():
         self.y = []
         self.z = []
 
+    def read_lightvec(self,dataset_num):
+        lightvec = []
+        i = dataset_num
+        #for i in range(2,11):
+        lightvec.clear()
+        self.clear_xyz()
+        print('show dataset lightvec: ', i)
+        i=i-2
+        file_name = '../'+self.dataset_names[i]+'/'+self.dataset_nums[i]+'/lightvec.txt'
+        file_name = './lightvec.txt'
+        print(file_name)
+        with open(file_name) as csvfile:
+            readCSV = csv.reader(csvfile, delimiter=' ')
+            for row in readCSV:
+                x = float(row[0])
+                y = float(row[1])
+                z = float(row[2])
+                self.x.append(x)
+                self.y.append(y)
+                self.z.append(z)
+                lightvec.append([x,y,z])
+        #self.plot()
+        #print('lightvec:\n',lightvec)
+
+        self.lightvec = np.asarray(lightvec)
+        print('lightvec.shape: ',self.lightvec.shape)
+        print(self.lightvec)
+
+    def clear_xyz(self):
+        self.x.clear()
+        self.y.clear()
+        self.z.clear()
+
     def plot(self):
-        for i in range(2,11):
-            self.x.clear()
-            self.y.clear()
-            self.z.clear()
-            print('show dataset lightvec: ', i)
-            i=i-2
-            file_name = '../'+self.dataset_names[i]+'/'+self.dataset_nums[i]+'/lightvec.txt'
-            print(file_name)
-            with open(file_name) as csvfile:
-                readCSV = csv.reader(csvfile, delimiter=' ')
-                for row in readCSV:
-                    self.x.append(float(row[0]))
-                    self.y.append(float(row[1]))
-                    self.z.append(float(row[2]))
-            fig = pyplot.figure()
-            ax = Axes3D(fig)
-            ax.scatter(self.x,self.y,self.z)
-            pyplot.show()
-            input('press enter to continue')
-            fig.clf()
+        fig = pyplot.figure()
+        ax = Axes3D(fig)
+        ax.scatter(self.x,self.y,self.z)
+        pyplot.show()
+        #input('press enter to continue')
+        #fig.clf()
 
     def dome(self, r, num_of_points):
         area = 4 * math.pi * r * r / (2 * num_of_points)
@@ -49,11 +68,7 @@ class LightVector():
         dphi = area / dv
         self.dome_points = np.array([])
         points = []
-
-        self.x.clear()
-        self.y.clear()
-        self.z.clear()
-
+        self.clear_xyz()
         for m in range(0,math.floor(Mv / 2)):
             v = math.pi * (m + 0.5) / Mv
             Mphi = math.floor (2 * math.pi * math.sin(v) * r / dphi)
@@ -69,16 +84,8 @@ class LightVector():
 
         self.dome_points = np.asarray(points)
         #print(self.dome_points)
-        print(self.dome_points.shape)
-
-        fig = pyplot.figure()
-        ax = Axes3D(fig)
-        ax.scatter(self.x,self.y,self.z)
-        pyplot.show()
-        input('press enter to continue')
-        fig.clf()
-
-
+        print('dome_points shape: ',self.dome_points.shape)
+        #self.plot()
 
     def icosahegron(self):
         self.t = (1 + math.sqrt(5))/2
@@ -138,33 +145,20 @@ class LightVector():
         print('dd =',self.dd)
         print(self.original_20_face_mid_points)
 
-        self.x.clear()
-        self.y.clear()
-        self.z.clear()
+        self.clear_xyz()
         for row in self.original_20_face_mid_points:
             self.x.append(float(row[0]))
             self.y.append(float(row[1]))
             self.z.append(float(row[2]))
-        fig = pyplot.figure()
-        ax = Axes3D(fig)
-        ax.scatter(self.x,self.y,self.z)
-        pyplot.show()
-        input('press Enter to see verticies:')
-        fig.clf()
-        self.x.clear()
-        self.y.clear()
-        self.z.clear()
+
+        self.plot()
+        self.clear_xyz()
         for row in self.original_12_vertices:
             self.x.append(float(row[0]))
             self.y.append(float(row[1]))
             self.z.append(float(row[2]))
         print(self.original_12_vertices)
-        fig = pyplot.figure()
-        ax = Axes3D(fig)
-        ax.scatter(self.x,self.y,self.z)
-        pyplot.show()
-        input('press Enter to end:')
-        fig.clf()
+        self.plot()
 
         # to keep using this method requires build a mesh datastructure
         # which I prefer not to do at this moment.
@@ -175,6 +169,6 @@ class LightVector():
 
 if __name__=="__main__":
     obj = LightVector()
-    #obj.plot()
-    #obj.icosahegron()
     obj.dome(1.0,310)
+    obj.read_lightvec(2)
+    #obj.icosahegron()
