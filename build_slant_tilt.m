3f8d1ffaa44470eab5afdb7348e1aa85036d9d34
hao <hao.coding@dorabot.com>
2018-04-27 10:36:40 +0800

    export to stl

--- build_slant_tilt.m
+++ build_slant_tilt.m
@@ -4,3 +4,6 @@ tn=normr(reshape(normal(:,:,1:2),row*column,2));
 tilt=reshape(acos(tn(:,1)),row,column);
 recsurf = shapeletsurf(slant, tilt, 6, 1, 2, 'slanttilt');
 surf(recsurf);
+X = deal(1:column);
+Y = deal(1:row);
+stlwrite('surf.stl',X,Y,recsurf,'mode','ascii')
